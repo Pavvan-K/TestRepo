3c03f28b022a70ce210a5fa0e027db6c8033eaf5
Pavvan-K <pavvands143@gmail.com>
2020-05-31 19:24:13 +0530

    Deleted

--- Feature2.py
+++ Feature2.py
@@ -1,5 +1,3 @@
 def Feature2():
-    print("This is feature2")
-    print("new change but this file not there in master")
-    print("updated readme.md")
-    print("Changes in branch2")
+    print ("New code added")
+
