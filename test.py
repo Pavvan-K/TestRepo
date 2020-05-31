cb571ccb7c06a8e4e45a3a75b461638f95ec30ac
Pavvan-K <pavvands143@gmail.com>
2020-05-31 13:17:04 +0530

    Local New Func added

--- test.py
+++ test.py
@@ -3,3 +3,6 @@ def testFunc():
     print("Modified in branch1 now")
     print("This i am commiting in local in branch1") 
     print("Commit 2 in branch2")
+
+git local_func():
+    print("Now I added this in local in master branch")
